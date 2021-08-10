e4019a8ea0a5e595891371131c117520122147c1
Mitchell Scobell <mitchscobell@gmail.com>
2021-08-10 17:52:16 -0500

    added validator package

--- src/notes-app/app.js
+++ src/notes-app/app.js
@@ -1,5 +1,9 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(validator.isEmail('mitch@scobellis.com'));
+console.log(validator.isURL('https://scobellis.com'));
